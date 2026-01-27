50ae9fb3bb7ad2b05ab723d95071a8184e405e13
Didier Grand <didier.grand@gmail.com>
2026-01-27 09:56:00 +0100

    Add editor styles

--- functions.php
+++ functions.php
@@ -394,3 +394,10 @@ function cmplz_show_banner_on_click() {
 	<?php
 }
 add_action( 'wp_footer', 'cmplz_show_banner_on_click' );
+
+function setup_editor_styles() {
+    add_theme_support('editor-styles');
+
+    add_editor_style('editor-styles.css');
+}
+add_action('after_setup_theme', 'setup_editor_styles');
